8990f97b53632a1a2f8ccc59ae67675d14a393f9
Valeria <valery0007@gmail.com>
2020-04-10 17:14:40 +0200

    adding to js

--- js/ratings.js
+++ js/ratings.js
@@ -1,8 +1,8 @@
 function collect_ratings() {
     let rating = 0;
-    let ratings = { count: 0, sum: 0, average: 0 };
+    const ratings = { count: 0, sum: 0, average: 0 };
 
-    let elements = document.querySelectorAll('.rating');
+    const elements = document.querySelectorAll('.rating');
 
     elements.foreach(element => {
         rating = element.id.replace('star', '');
