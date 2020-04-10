f2fd5b30c72c24723dc26004dfc576eadaa3fed1
Valeria <valery0007@gmail.com>
2020-04-10 17:32:50 +0200

    fixing err

--- js/ratings.js
+++ js/ratings.js
@@ -1,10 +1,10 @@
 function collect_ratings() {
-    let rating = 0;
-    const ratings = { count: 0, sum: 0, average: 0 };
+    const ratings = { 'count': 0, 'sum': 0, 'average': 0 };
 
+    let rating = 0;
     const elements = document.querySelectorAll('.rating');
 
-    elements.foreach(element => {
+    elements.forEach(element => {
         rating = element.id.replace('star', '');
     });
 
